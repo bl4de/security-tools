5c813926d719a3f56e002a08a7722094238f6595
Rafal 'bl4de' Janicki <bloorq@gmail.com>
2014-09-18 21:58:04 +0200

    find comments in HTML file

--- htmlanalyzer.py
+++ htmlanalyzer.py
@@ -2,13 +2,26 @@
 #
 # HTML source analyzer
 #
+import sys
 
 def main():
-    _file = open("index.html", "r")
-
+    _file = open(sys.argv[1], "r")
+    i = 0
+    print "HTML Analyze"
+    print "============"
+    print
     for _line in _file:
-        print _line
+        i = i + 1
+        _analyzeLine(_line, i)
         
+        
+def _analyzeLine(_line, i):
+    if "<!--" in _line:
+        print "comment found in line %d" % (i)
+    
 # main program
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 2:
+        main()
+    else:
+        print "input HTML file name"
