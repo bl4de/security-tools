6fea5a11fb497dc766c7eb0c7de90f854ab54c17
Rafal 'bl4de' Janicki <bloorq@gmail.com>
2014-09-19 00:52:18 +0200

    'admin' string searching

--- htmlanalyzer.py
+++ htmlanalyzer.py
@@ -14,14 +14,17 @@ def main():
         i = i + 1
         _analyzeLine(_line, i)
         
-        
+# find interesting string(s)
 def _analyzeLine(_line, i):
     if "<!--" in _line:
-        print "comment found in line %d" % (i)
+        print "comment found at line %d: %s" % (i, _line)
+    if "admin" in _line:
+        print "'admin' string found at line: %d" % (i)
+    
     
 # main program
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         main()
     else:
-        print "input HTML file name"
+        print "Enter HTML file name"
