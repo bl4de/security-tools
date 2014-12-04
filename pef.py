78064a4c85fe9e7c9c2fb0cfd94b50c1e07ab924
Rafal 'bl4de' Janicki <bloorq@gmail.com>
2014-12-04 23:21:40 +0100

    reformat; code analyze errors fixed

--- pef.py
+++ pef.py
@@ -6,60 +6,62 @@
 import sys
 import os
 
+
 class _PefOutput:
+    def __init__(self):
+        pass
+
     Black = '\33[30m'
     Red = '\33[31m'
     Green = '\33[32m'
     Yellow = '\33[33m'
-    Blue ='\33[34m'
+    Blue = '\33[34m'
     Magenta = '\33[35m'
     Cyan = '\33[36m'
     White = '\33[37m'
     _endline = '\33[0m'
-    
+
     efMsgFound = "exploitable function call"
     efMsgGlobalFound = "global variable explicit call"
 
 # exploitable functions
-exploitableFunctions = [" system"," exec"," popen", " pcntl_exec",
-" eval"," preg_replace"," create_function", " include"," require"," passthru"," shell_exec"," popen"," proc_open",
-" pcntl_exec"," asset"," extract"," parse_str"," putenv"," ini_set"," mail"," header"]
+exploitableFunctions = [" system", " exec", " popen", " pcntl_exec",
+                        " eval", " preg_replace", " create_function", " include", " require", " passthru",
+                        " shell_exec", " popen", " proc_open",
+                        " pcntl_exec", " asset", " extract", " parse_str", " putenv", " ini_set", " mail", " header"]
 
 # dangerous global(s)
-globalVars = ["$_POST", "$_GET", "$_COOKIE", "$_REQUEST", "$_SERVER"];
-
+globalVars = ["$_POST", "$_GET", "$_COOKIE", "$_REQUEST", "$_SERVER"]
 
 
 # prints formated output line
 def printSrcCodeLine(_line, i, _fn, _message):
     print "::  line %d :: \33[33;1m%s\33[0m %s found " % (i, _fn, _message)
     print _PefOutput.Blue + _line + _PefOutput._endline
-    
-    
+
+
 # performs code analysis, line by line
 def main(__fileToAnalise):
-    
-    
     # open file to analyse
     _file = open(__fileToAnalise, "r")
     i = 0
     total = 0
-    fileNameLength = len(__fileToAnalise)
-    lineLength = 97
-    
-    print "-" * 14," FILE: \33[33m%s\33[0m " % (__fileToAnalise), "-" * (lineLength - fileNameLength - 21)  , "\n"
+    filenamelength = len(__fileToAnalise)
+    linelength = 97
+
+    print "-" * 14, " FILE: \33[33m%s\33[0m " % __fileToAnalise, "-" * (linelength - filenamelength - 21), "\n"
 
     for _line in _file:
-        i = i + 1
+        i += 1
         for _fn in exploitableFunctions:
             if _fn + '(' in _line or _fn + ' (' in _line:
-                total = total + 1
+                total += 1
                 printSrcCodeLine(_line, i, _fn + '()', _PefOutput.efMsgFound)
         for _global in globalVars:
             if _global in _line:
-                total = total + 1
+                total += 1
                 printSrcCodeLine(_line, i, _global, _PefOutput.efMsgGlobalFound)
-        
+
     if total < 1:
         print _PefOutput.Green + "No exploitable functions found\n" + _PefOutput._endline
     else:
@@ -72,8 +74,7 @@ if __name__ == "__main__":
         print "-" * 6, " PHP Exploitable functions scanner", " " * 41, "-" * 16
         print "-" * 6, " GitHub: bl4de | Twitter: @_bl4de | bloorq@gmail.com ", " " * 22, "-" * 16
         print "-" * 100, "\33[0m\n"
-        
-        
+
         # main program loop
         if len(sys.argv) == 3 and sys.argv[1] == "-R":
             file_list = os.listdir(sys.argv[2])
@@ -82,8 +83,7 @@ if __name__ == "__main__":
                     main(__file)
         else:
             main(sys.argv[1])
-            
-            
+
         print
     else:
         print "Enter PHP or directory name with file(s) to analyse"
