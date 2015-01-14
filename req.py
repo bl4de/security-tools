85adcd8d0cc5423e4178505822af2dcf76203bc7
Rafal 'bl4de' Janicki <bloorq@gmail.com>
2015-01-14 16:27:32 +0100

    Refactoring

--- req.py
+++ req.py
@@ -7,14 +7,16 @@
 import requests
 import sys
 
+
 def main(url):
     if 'http://' not in url:
         url = 'http://' + url
-    
+
     req = requests.get(url)
     print req.headers
-    
+
+
 if __name__ == '__main__':
     url = sys.argv[1]
-    
+
     main(url)
