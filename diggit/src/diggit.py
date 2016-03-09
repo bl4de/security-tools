4d054c175348ebfa307fb79ccc61e98356435aa8
bloorq@gmail.com <bloorq@gmail.com>
2016-03-09 19:54:21 +0000

    diggit.py - download object, display type and details

--- diggit/src/diggit.py
+++ diggit/src/diggit.py
@@ -3,43 +3,75 @@
 # bl4de | <bloorq@gmail.com> | https://twitter.com/_bl4de
 #
 # diggit - gets .git repository
-import sys
 import os
+import sys
 
 # some common definitions
 VERSION = "0.0.1"
-BASE_DIR = ".git/"
-OBJECT_DIR = BASE_DIR + "objects/"
+OBJECT_DIR = ".git/objects/"
 
 DESTINATION_FOLDER = "/Users/bl4de/hacking/playground/diggit/test1/"
 
+Black = '\33[30m'
+Red = '\33[31m'
+Green = '\33[32m'
+Yellow = '\33[33m'
+Blue = '\33[34m'
+Magenta = '\33[35m'
+Cyan = '\33[36m'
+White = '\33[37m'
+_endline = '\33[0m'
 
-def get_object_url(object_hash):
+
+def print_credits():
+    print "#" * 68
+    print "###    diggit.py  |  Twitter: @_bl4de  | GitHub: bl4de           ###"
+    print "#" * 68
+
+
+def print_object_details(object_type, object_content):
+    print "\n" + Cyan + "#" * 12 + " " + object_hash \
+          + " information " + "#" * 12 + _endline
+    print "\n{0}[*] Object type: {3}{2}{1}{3}".format(Green, object_type, Red,
+                                                      _endline)
+    print "{0}[*] Object content:{1}\n".format(Green, _endline)
+    print "{0}{1}{2}".format(Yellow, object_content, _endline)
+
+    print "\n" + Cyan + "#" * 78 + _endline
+
+
+def get_object_url():
     return OBJECT_DIR + object_hash[0:2] + "/" + object_hash[2:]
 
 
-def get_object_dir_prefix(object_hash):
+def get_object_dir_prefix():
     return object_hash[0:2] + "/"
 
 
-def save_git_object(object_hash):
-    complete_url = base_url + "/" + get_object_url(object_hash)
+def save_git_object():
+    complete_url = base_url + "/" + get_object_url()
 
-    os.system("curl '" + complete_url + "' --create-dirs -o '" +
-              DESTINATION_FOLDER +
-              get_object_url(object_hash) + "'")
+    os.system("curl --silent '" + complete_url + "' --create-dirs -o '" +
+              DESTINATION_FOLDER + get_object_url() + "'")
 
     git_object_type = os.popen("cd " + DESTINATION_FOLDER + OBJECT_DIR +
-                               get_object_dir_prefix(object_hash) +
+                               get_object_dir_prefix() +
                                " && git cat-file -t " + object_hash).read()
 
-    print git_object_type
+    git_object_content = os.popen("cd " + DESTINATION_FOLDER + OBJECT_DIR +
+                                  get_object_dir_prefix() +
+                                  " && git cat-file -p " + object_hash).read()
+    print_object_details(git_object_type, git_object_content)
 
 
 # main program
 if __name__ == "__main__":
-    print "### diggit.py ver. {v} ###".format(v=VERSION)
 
-base_url = sys.argv[1]
+    # domain, base path for .git folder, eg. http://website.com
+    base_url = sys.argv[1]
 
-save_git_object('07603070376d63d911f608120eb4b5489b507692')
+    # hash of object to save
+    object_hash = sys.argv[2]
+
+    if base_url and object_hash:
+        save_git_object()
