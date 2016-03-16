fc014b6be1412c59e4c745df516890e8177e5c40
bloorq@gmail.com <bloorq@gmail.com>
2016-03-16 23:50:42 +0000

    Cleanup; add some new files

---
+++ pef/imports/pefdefs.py
@@ -0,0 +1,15 @@
+# Definitions for pef.py
+
+# exploitable functions
+exploitableFunctions = [" system", " exec", " popen", " pcntl_exec",
+                        " eval", " preg_replace", " create_function", " include", " require", " passthru",
+                        " shell_exec", " popen", " proc_open",
+                        " pcntl_exec", " asset", " extract", " parse_str", " putenv", " ini_set", " mail", " header"]
+
+# dangerous global(s)
+globalVars = ["$_POST", "$_GET", "$_COOKIE", "$_REQUEST", "$_SERVER"]
+
+# dangerous patterns - LFI/RFI
+fileInclude = ["include($_GET", "require($_GET", 
+"include_once($_GET", "require_once($_GET",
+"include($_REQUEST", "require($_REQUEST","include_once($_REQUEST","require_once($_REQUEST"]
