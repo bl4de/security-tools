2ec833359e40195998ea236d6333fb106a64c840
bloorq@gmail.com <bloorq@gmail.com>
2016-07-22 22:21:11 +0100

    refactoring; move console output beautifier to separate module

--- htmlanalyzer/htmlanalyzer.py
+++ htmlanalyzer/htmlanalyzer.py
@@ -5,44 +5,11 @@
 #
 import sys
 
+from modules.console_output_beautifier import ConsoleOutputBeautifier
+
 
 # TODO: Python doc in functions and for module
 # TODO: args parser for: -c (comments) -s (links/src), -j (JavaScript)
-
-class ConsoleOutputBeautifier:
-    """This class defines properties and methods to manipulate console output"""
-    colors = {
-        "black": '\33[30m',
-        "white": '\33[37m',
-        "red": '\33[31m',
-        "green": '\33[32m',
-        "yellow": '\33[33m',
-        "blue": '\33[34m',
-        "magenta": '\33[35m',
-        "cyan": '\33[36m'
-    }
-
-    characters = {
-        "endline": '\33[0m'
-    }
-
-    def __init__(self):
-        return None
-
-    @staticmethod
-    def getColor(color_name):
-        """returns color identified by color_name or white as default value"""
-        if color_name in ConsoleOutputBeautifier.colors:
-            return ConsoleOutputBeautifier.colors[color_name]
-        return ConsoleOutputBeautifier.colors["white"]
-
-    @staticmethod
-    def getSpecialChar(char_name):
-        """returns special character identified by char_name"""
-        if char_name in ConsoleOutputBeautifier.characters:
-            return ConsoleOutputBeautifier.characters[char_name]
-        return ""
-
 
 def main():
     _ident = ""
