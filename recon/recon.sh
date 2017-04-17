8f380dccd69144e7bad6edfbe631bcbe5718a46b
bloorq@gmail.com <bloorq@gmail.com>
2017-04-17 22:23:22 +0100

    recon.sh for automate host recon (nmap/nikto/theharvester)

---
+++ recon/recon.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# r3c0n.sh by bl4de | twitter.com/bl4de
+
+TARGET=$1
+OUTPUT_FILE="$TARGET".log
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+LGRAY='\033[0;37m'
+
+NC='\033[0m' # No Color
+
+echo -e "${BLUE}[+]${NC} ${GREEN}nmap scanning...${NC}"
+nmap -p- -A -sV $TARGET -o $OUTPUT_FILE 
+
+echo -e "${BLUE}[+]${NC}  ${GREEN}nikto scanning...${NC}"
+/Users/bl4de/hacking/tools/nikto/program/nikto.pl -host $TARGET >> $OUTPUT_FILE 
+
+echo -e "${BLUE}[+]${NC}  ${GREEN}theHarvester scanning...${NC}"
+theharvester -d $TARGET -b all >> $OUTPUT_FILE 
