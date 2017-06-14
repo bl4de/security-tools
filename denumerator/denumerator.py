522fb69e71e848ea2d965b8e0934f3d03018d639
bloorq@gmail.com <bloorq@gmail.com>
2017-06-15 00:27:30 +0100

    syntax improvement

--- denumerator/denumerator.py
+++ denumerator/denumerator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-welcome = """
+"""
 --- dENUMerator ---
 
 by bl4de | bloorq@gmail.com | Twitter: @_bl4de | HackerOne: bl4de
@@ -13,18 +13,31 @@ usage:
 $ ./denumerator.py [domain_list_file]
 """
 
-import requests
 import sys
+import requests
+
+welcome = """
+--- dENUMerator ---
+usage:
+$ ./denumerator.py [domain_list_file]
+"""
 
 requests.packages.urllib3.disable_warnings()
 
-allowed_http_responses = [200,302,304,401,404,403,500]
+allowed_http_responses = [200, 302, 304, 401, 404, 403, 500]
+
 
 def usage():
+    """
+    prints welcome message
+    """
     print welcome
 
 
 def send_request(proto, domain):
+    """
+    sends request to check if server is alive
+    """
     protocols = {
         'http': 'http://',
         'https': 'https://'
@@ -33,9 +46,7 @@ def send_request(proto, domain):
                         timeout=5,
                         allow_redirects=False,
                         verify=False,
-                        headers={
-                            'Host': domain
-    })
+                        headers={'Host': domain})
 
     if resp.status_code in allowed_http_responses:
         print '[+] domain {}:\t\t HTTP {}'.format(domain, resp.status_code)
@@ -43,14 +54,16 @@ def send_request(proto, domain):
     return resp.status_code
 
 
-def enumerate_domains(domains, output_file):
-    i = len(domains)
+def enumerate_domains(domains):
+    """
+    enumerates domain from domains
+    """
     for d in domains:
         try:
             d = d.strip('\n').strip('\r')
             return_code = send_request('http', d)
             # if http not working, try https
-            if (return_code not in allowed_http_responses):
+            if return_code not in allowed_http_responses:
                 send_request('https', d)
 
         except requests.exceptions.InvalidURL:
@@ -68,13 +81,13 @@ def enumerate_domains(domains, output_file):
             pass
 
 
-if (len(sys.argv) < 2):
+if len(sys.argv) < 2:
     print welcome
     exit(0)
 
 domains = open(sys.argv[1].strip(), 'rw').readlines()
 output_file = open('denumerator-{}-output.txt'.format(domains[0].strip()), 'w')
 
-enumerate_domains(domains, output_file)
+enumerate_domains(domains)
 
 output_file.close()
