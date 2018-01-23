7fc7276eb6ad5f2759b3fce536d95e77354fdf45
rafal.janicki@genomicsmed.ie <rafal.janicki@genomicsmed.ie>
2018-01-23 15:28:11 +0000

    [hasher.py] URL encode string

--- hasher.py
+++ hasher.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
-
+#!/usr/bin/python
 ### created by bl4de | bloorq@gmail.com | twitter.com/_bl4de    ###
 ### github.com/bl4de | hackerone.com/bl4de                      ###
 
 import sys
 import hashlib
+import urllib
 import base64
 
 description = """
@@ -23,6 +23,7 @@ def main(s):
     print "[+] SHA1\t\t{}".format(hashlib.sha1(s).hexdigest())
     print "[+] MD5 \t\t{}".format(hashlib.md5(s).hexdigest())
     print "[+] Base64 \t\t{}".format(base64.b64encode(s))
+    print "[+] URL-encoded \t{}".format(urllib.pathname2url(s))
 
 
 if __name__ == "__main__":
