3978d5081a1e79d156748a2e6a527919497b4cb7
bloorq@gmail.com <bloorq@gmail.com>
2018-01-27 15:21:17 +0000

    [nodestructor] Refactoring; show only files with identified patterns

--- nodestructor/nodestructor.py
+++ nodestructor/nodestructor.py
@@ -29,48 +29,12 @@ PATTERNS = [
     ".*f.readFile\(",
     ".*bodyParser()",
     ".*handlebars.SafeString\(",
-    ".*eval\(",
-    ".*(eval\()(.{0,40000})(req\.|req\.query|req\.body|req\.param)",
-    ".*(setTimeout\()(.{0,40000})(req\.|req\.query|req\.body|req\.param)",
-    ".*(setInterval\()(.{0,40000})(req\.|req\.query|req\.body|req\.param)",
-    ".*(new Function\()(.{0,40000})(req\.|req\.query|req\.body|req\.param)",
-    ".*(deserialize\(|unserialize\()",
-    ".*(require\('js-yaml'\)\.load\(|yaml\.load\()",
-    ".*(\[)*('|\")*NODE_TLS_REJECT_UNAUTHORIZED('|\")*(\])*(\s)*=(\s)*('|\")*0('|\")*",
-    ".*noEscape(\s)*:(\s)*true",
-    ".*SSL_VERIFYPEER(\s)*:(\s)*0",
-    ".*createHash\(('|\")md5('|\")",
-    ".*createHash\(('|\")sha1('|\")",
-    ".*password\s*=\s*['|\"].+['|\"]\s{0,5}[;|.]",
-    ".*\s*['|\"]password['|\"]\s*:",
-    ".*\s*['|\"]+secret['|\"]+\s*:|\s*secret\s*:\s*['|\"]+",
-    ".*username\s*=\s*['|\"].+['|\"]\s{0,5}[;|.]",
-    ".*lusca.xssProtection\(false\)|X-XSS-Protection('|\")*(\s)*(:|,)(\s)*('|\")*0",
-    ".*(\.createReadStream\()(.{0,40000})(req\.|req\.query|req\.body|req\.param)",
-    ".*(\.readFile\()(.{0,40000})(req\.|req\.query|req\.body|req\.param)",
-    ".*(res\.redirect\()( *)(req\.|req\.query|req\.body|req\.param)",
-    ".*(SELECT|INSERT|UPDATE|DELETE|CREATE|EXPLAIN)(.{0,40000})(req\.|req\.query|req\.body|req\.param)",
-    ".*(\.)(find|drop|create|explain|delete|count|bulk|copy)(.{0,4000})({(.{0,4000})\$where:)(.{0,4000})(req\.|req\.query|req\.body|req\.param)",
-    ".*(res\.(write|send)\()(.{0,40000})(req\.|req\.query|req\.body|req\.param)",
-    ".*(res\.set\()(.{0,40000})(req\.|req\.query|req\.body|req\.param)",
-    ".*{{{\s*[\w.\[\]\(\)]+\s*}}}",
-    ".*{\s*[\w.\[\]\(\)]+\s*\|\s*s\s*}",
-    ".*#{\s*[\w.\[\]\(\)\'\"]+\s*}",
-    ".*&lt;%-\s*[\w.\[\]\(\)]+\s*%&gt;",
-    ".*&lt;%-\s*@+[\w.\[\]\(\)]+\s*%&gt;",
-    ".*require( )*(\()( *)('|\")request('|\")( *)(\))",
-    ".*require( )*(\()( *)('|\")request('|\")( *)(\))",
-    ".*require( )*(\()( *)('|\")needle('|\")( *)(\))",
-    ".*require\(('|\")helmet-csp('|\")\)|helmet.csp|lusca.csp\(|Content-Security-Policy",
-    ".*helmet.xframe|lusca.xframe\(|require\(('|\")frameguard('|\")\)|frameguard\(|X-Frame-Options",
-    ".*helmet.hsts\({|lusca.hsts\({|hsts\({|require\(('|\")hsts('|\")\)|Strict-Transport-Security",
-    ".*Public-Key-Pins(:|,)*",
-    ".*helmet.xssFilter\(\)|lusca.xssProtection\(true\)|X-XSS-Protection('|\")*(\s)*(:|,)(\s)*('|\")*1",
-    ".*helmet.noSniff|require\(('|\")dont-sniff-mimetype('|\")\)|nosniff\(\)|X-Content-Type-Options('|\")*(\s)*(:|,)(\s)*('\"\")*nosniff",
-    ".*require\(('|\")ienoopen('|\")\)|ienoopen\(|helmet.ienoopen\(|X-Download-Options('|\")*(\s)*(:|,)(\s)*('\"\")*noopen",
-    "httpOnly(\s)*:(\s)*true|httpOnly",
-    ".disable\(('|\")x-powered-by('|\")\)|require\(('|\")hide-powered-by('|\")\)|hidePoweredBy\(|helmet.hidePoweredBy\(|removeHeader\(('|\")X-Powered-By('|\")\)"
+    ".*eval\("
 ]
+
+total_files = 0
+patterns_identified = 0
+files_with_identified_patterns = 0
 
 
 def show_banner():
@@ -87,7 +51,7 @@ def printcodeline(_line, i, _fn, _message):
     Formats and prints line of output
     """
     print "::  line %d :: \33[33;1m%s\33[0m %s " % (i, _fn, _message)
-    print beautyConsole.getColor("lightgrey") + _line + \
+    print beautyConsole.getColor("grey") + _line + \
         beautyConsole.getSpecialChar("endline")
 
 
@@ -102,14 +66,12 @@ def main(src):
     performs code analysis, line by line
     """
     global patterns_identified
+    global files_with_identified_patterns
+    print_filename = True
 
     _file = open(src, "r")
     i = 0
     patterns_found_in_file = 0
-    filenamelength = len(src)
-    linelength = 97
-
-    print "FILE: \33[33m%s\33[0m " % src
 
     for _line in _file:
         i += 1
@@ -117,17 +79,17 @@ def main(src):
         for __pattern in PATTERNS:
             __rex = re.compile(__pattern)
             if __rex.match(__line):
+                if print_filename:
+                    files_with_identified_patterns = files_with_identified_patterns + 1
+                    print "FILE: \33[33m{}\33[0m\n".format(src)
+                    print_filename = False
                 patterns_found_in_file += 1
                 printcodeline(_line, i, __pattern + ')',
                               ' dangerous pattern identified: ')
 
-    if patterns_found_in_file < 1:
-        print beautyConsole.getColor("green") + \
-            "No patterns identified\n" + \
-            beautyConsole.getSpecialChar("endline")
-    else:
+    if patterns_found_in_file > 0:
         patterns_identified = patterns_identified + patterns_found_in_file
-        print "\n\n" + beautyConsole.getColor("red") + \
+        print beautyConsole.getColor("red") + \
             "Identified %d code pattern(s)\n" % (patterns_found_in_file) + \
             beautyConsole.getSpecialChar("endline")
         print beautyConsole.getColor("white") + "-" * 100
@@ -135,8 +97,6 @@ def main(src):
 
 # main program
 if __name__ == "__main__":
-    total_files = 0
-    patterns_identified = 0
 
     if len(sys.argv) >= 2:
         show_banner()
@@ -167,7 +127,7 @@ if __name__ == "__main__":
         print " {} file(s) scanned in total".format(total_files)
         if patterns_identified > 0:
             print beautyConsole.getColor(
-                "red"), "Identified {} code pattern(s) in total".format(patterns_identified)
+                "red"), "Identified {} code pattern(s) in {} file(s)".format(patterns_identified, files_with_identified_patterns)
         print beautyConsole.getColor("white")
 
     else:
