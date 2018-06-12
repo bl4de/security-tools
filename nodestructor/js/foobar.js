b1a06503dcc08b5ba285f339e440c744c11c5115
bloorq@gmail.com <bloorq@gmail.com>
2018-06-12 23:50:53 +0100

    [nodestructor] basic static analysis IN PROGRESS

--- nodestructor/js/foobar.js
+++ nodestructor/js/foobar.js
@@ -1,18 +1,10 @@
+
 'use strict'
 
-// some sophisticated function
-function bar(arg) {
-    return arg + 10
+function fn(arg) {
+    return arg
 }
 
-// initialize constant
-const $BAZ = "heyyy!"
+let x = 20
+x = fn(x)
 
-// initialize foo
-let __foo = 10
-
-// foo is changed by bar()
-__foo = bar(__foo)
-
-// foo = 20
-console.log(__foo)
--- nodestructor/static_analysis.py
+++ nodestructor/static_analysis.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import re
 
+from imports.beautyConsole import beautyConsole
+
 jsfile = open('js/foobar.js', 'r')
 file_read = jsfile.readlines()
 
@@ -12,6 +14,13 @@ variable_definition_regex = '(let|const|var)\s+([_$a-zA-Z0-9]+)'
 
 line_no = 0
 
+
+def decorate_varname(var_name):
+    return beautyConsole.getColor("red") + var_name + beautyConsole.getColor("white")
+
+def decorate_source(src_line):
+    return beautyConsole.getColor("yellow") + src_line + beautyConsole.getColor("white")
+
 print "\n\n[+] STARTING ANALYSIS...\n"
 for line in file_read:
     line = line.replace('\n', '')
@@ -21,13 +30,15 @@ for line in file_read:
     res = re.search(variable_definition_regex, line)
     if res:
         variable_name = res.group(2)
-        print "\n[+] found {} variable definition in line {}: {}".format(variable_name, line_no, line)
+        print "\n[+] found {} variable definition in line {}: {}".format(decorate_varname(variable_name), line_no, decorate_source(line))
         inner_line_no = 0
 
         for inner_line in file_read:
             inner_line = inner_line.replace('\n', '')
             inner_line_no = inner_line_no + 1
-            if variable_name in inner_line:
-                print "   {} used in expression in line {}: {}".format(variable_name, inner_line_no, inner_line)
+            if variable_name + '=' in inner_line.replace(' ', ''):
+                print "\t{} used in assignment in line {}:\n\t{}\n".format(decorate_varname(variable_name), inner_line_no, decorate_source(inner_line))
+            if '({})'.format(variable_name) in inner_line.replace(' ', ''):
+                print "\t{} used in function call as an argument in line {}:\n\t{}\n".format(decorate_varname(variable_name), inner_line_no, decorate_source(inner_line))
 
 print "\n\n[+] DONE\n"
