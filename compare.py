6f5dafc10a5bef20dcee26b1eaf9414e2b77bca7
rafal.janicki@genomicsmed.ie <rafal.janicki@genomicsmed.ie>
2018-07-16 15:02:31 +0100

    [compare.py] improvements; refactoring; work in progress...

--- compare.py
+++ compare.py
@@ -6,38 +6,39 @@ Creates snapshots and checksums of websites
 Compares snapshots
 Shows changes between snapshots
 """
-
 import requests
-import base64
-import time
 import os
 
-site_url = 'https://range.roche.net/'
+site_url = os.sys.argv[1]
+snapshot_filename = site_url.replace('https://','').replace('http://','').replace('/','_').replace(':','_').replace('.','_') + '_SNAPSHOT'
+differencies = 0
+
 res = requests.get(site_url)
 
-if os.path.isfile('snapshot.b64'):
+if os.path.isfile(snapshot_filename):
     print '\n[+] snapshot found, compare snapshot with current source...'
-    snapshot_source = open('snapshot.b64', 'r').readlines()
+    snapshot_source = open(snapshot_filename, 'r').readlines()
     site_source = res.text.encode('utf-8').split('\n')
     line_counter = 0
-    differencies = 0
     
     for snapshot_line in snapshot_source:
         # print 'snapshot:  ' + snapshot_line.strip()
         # print 'source:    ' + site_source[line_counter].strip()
 
         if snapshot_line.strip() != site_source[line_counter].strip():
-            print '  >>> snaphsot line {}: '.format(line_counter + 1)
+            print '>>>>>     snaphsot line {}: '.format(line_counter + 1)
             print snapshot_line
-            print '  <<< site source line {}'.format(line_counter + 1)
+            print '\n<<<<<     site source line {}'.format(line_counter + 1)
             print site_source[line_counter]
+
+            print '\n--------------------------------------------------------------------------------------------------\n'
             differencies = differencies + 1
 
         line_counter = line_counter + 1
 
 else:
     print '\n[+] snapshot not found, saving snapshot...'
-    f = open('snapshot.b64', 'w')
+    f = open(snapshot_filename, 'w')
     f.write(res.text.encode('utf-8'))
 
 if differencies > 0:
