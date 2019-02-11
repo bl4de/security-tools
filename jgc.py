c8362c16ce6558b01a2293e4c6327a965e568f91
rafal.janicki@genomicsmed.ie <rafal.janicki@genomicsmed.ie>
2019-02-11 15:26:10 +0000

    Jenkins Groovy Console cmd runner

---
+++ jgc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import requests
+import sys
+
+print """
+Jenkins Groovy Console cmd runner.
+
+usage: ./jgc.py [HOST]
+
+Then type any command and wait for STDOUT output from remote machine.
+Type 'exit' to exit :)
+"""
+URL = sys.argv[1] + '/scriptText'
+HEADERS = {
+    'User-Agent': 'jgc'
+}
+
+while 1:
+    CMD = raw_input(">> Enter command to execute (or type 'exit' to exit): ")
+    if CMD == 'exit':
+        print "exiting...\n"
+        exit(0)
+
+    DATA = {
+        'script': 'println "{}".execute().text'.format(CMD)
+    }
+    result = requests.post(URL, headers=HEADERS, data=DATA)
+    print result.text
