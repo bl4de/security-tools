ef78f7c6d0e990372466cf472a33bfcbc57609fb
bloorq@gmail.com <bloorq@gmail.com>
2019-07-18 00:28:27 +0100

    Refactoring pef.py

--- denumerator/denumerator.py
+++ denumerator/denumerator.py
@@ -48,7 +48,7 @@ colors = {
 }
 
 requests.packages.urllib3.disable_warnings()
-allowed_http_responses = [200, 204, 403, 404, 405, 415, 422, 500]
+allowed_http_responses = [200, 302, 403, 404, 405, 415, 422, 500]
 timeout = 2
 
 
--- pef/pef.py
+++ pef/pef.py
@@ -53,17 +53,19 @@ class PefEngine:
             "medium": 0,
             "low": 0
         }
+
+        self.header_printed = False
         return
 
     def header_print(self, file_name, header_print):
         """
         prints file header
         """
-        if header_printed == False:
+        if self.header_printed == False:
             print beautyConsole.getColor("white") + "-" * 100
             print "FILE: \33[33m%s\33[0m " % os.path.realpath(file_name), "\n"
-            header_printed = True
-        return header_printed
+            self.header_printed = True
+        return self.header_printed
 
     def print_code_line(self, _line, i, fn, prev_line="", next_line="", prev_prev_line="", next_next_line="", severity={}, verbose=False):
         """
@@ -132,7 +134,7 @@ class PefEngine:
         linelength = 97
         all_lines = f.readlines()
 
-        header_printed = False
+        self.header_printed = False
         prev_prev_line = ""
         prev_line = ""
         next_line = ""
@@ -159,10 +161,11 @@ class PefEngine:
                     # also, it has to checked agains @ at the beginning of the function name
                     # @ prevents from output being echoed
                     if fn in line or atfn in line:
-                        header_printed = header_print(f.name, header_printed)
+                        self.header_printed = self.header_print(
+                            f.name, self.header_printed)
                         total += 1
                         self.print_code_line(l, i, fn + (')' if '(' in fn else ''), prev_line,
-                                             next_line, prev_prev_line, next_next_line, severity,
+                                             next_line, prev_prev_line, next_next_line, self.severity,
                                              verbose)
             else:
                 for fn in pefdefs.exploitableFunctions:
@@ -172,10 +175,11 @@ class PefEngine:
                     # also, it has to checked agains @ at the beginning of the function name
                     # @ prevents from output being echoed
                     if fn in line or atfn in line:
-                        header_printed = header_print(f.name, header_printed)
+                        self.header_printed = self.header_print(
+                            f.name, self.header_printed)
                         total += 1
                         self.print_code_line(l, i, fn + (')' if '(' in fn else ''), prev_line,
-                                             next_line, prev_prev_line, next_next_line, severity,
+                                             next_line, prev_prev_line, next_next_line, self.severity,
                                              verbose)
 
             if critical == False:
@@ -184,37 +188,40 @@ class PefEngine:
                     dp = " {}".format(dp)
                     # remove spaces to allow detection eg. include(  $_GET['something]  )
                     if dp in line.replace(" ", ""):
-                        header_printed = header_print(f.name, header_printed)
+                        self.header_printed = self.header_print(
+                            f.name, self.header_printed)
                         total += 1
                         self.print_code_line(l, i, dp + '()', prev_line, next_line,
-                                             prev_prev_line, next_next_line, severity,
+                                             prev_prev_line, next_next_line, self.severity,
                                              verbose)
 
                 for globalvars in pefdefs.globalVars:
                     if globalvars in line:
-                        header_printed = header_print(f.name, header_printed)
+                        self.header_printed = self.header_print(
+                            f.name, self.header_printed)
                         total += 1
                         self.print_code_line(l, i, globalvars, prev_line, next_line,
-                                             prev_prev_line, next_next_line, severity,
+                                             prev_prev_line, next_next_line, self.severity,
                                              verbose)
 
                 for refl in pefdefs.reflectedProperties:
                     if refl in line:
-                        header_printed = header_print(f.name, header_printed)
+                        self.header_printed = self.header_print(
+                            f.name, self.header_printed)
                         total += 1
                         self.print_code_line(l, i, refl, prev_line, next_line,
-                                             prev_prev_line, next_next_line, severity,
+                                             prev_prev_line, next_next_line, self.severity,
                                              verbose)
 
                 if sql == True:
                     for refl in pefdefs.otherPatterns:
                         p = re.compile(refl)
                         if p.search(l):
-                            header_printed = header_print(
-                                f.name, header_printed)
+                            self.header_printed = self.header_print(
+                                f.name, self.header_printed)
                             total += 1
                             self.print_code_line(l, i, refl, prev_line, next_line,
-                                                 prev_prev_line, next_next_line, severity,
+                                                 prev_prev_line, next_next_line, self.severity,
                                                  verbose)
 
         if total < 1:
@@ -226,190 +233,38 @@ class PefEngine:
 
         return total  # return how many findings in current file
 
-        def run(self):
-            """
-            runs scanning
-            """
-            if self.recursive:
-                for root, subdirs, files in os.walk(self.filename):
-                    for f in files:
-                        self.scanned_files = self.scanned_files + 1
-                        res = self.main(os.path.join(root, f))
-                        self.found_entries = self.found_entries + res
-            else:
-                self.scanned_files = self.scanned_files + 1
-                self.found_entries = self.main(self.filename)
-            return
-
-
-def printcodeline(_line, i, fn, prev_line="", next_line="", prev_prev_line="", next_next_line="", severity={}, verbose=False):
-    """
-    Formats and prints line of output
-    """
-    impact_color = {
-        "low": "green",
-        "medium": "yellow",
-        "high": "red"
-    }
-
-    if verbose == True:
-        print " line %d :: \33[33;1m%s\33[0m " % (i, fn)
-    else:
-        print "{}line {} :: {}{} ".format(beautyConsole.getColor(
-            "white"), i, beautyConsole.getColor("grey"), _line.strip())
-
-    # print legend only if there i sentry in pefdocs.py
-    if fn and fn.strip() in pefdocs.exploitableFunctionsDesc.keys():
-        impact = pefdocs.exploitableFunctionsDesc.get(fn.strip())[3]
-        description = pefdocs.exploitableFunctionsDesc.get(fn.strip())[
-            0]
-        syntax = pefdocs.exploitableFunctionsDesc.get(fn.strip())[1]
-        vuln_class = pefdocs.exploitableFunctionsDesc.get(fn.strip())[2]
-
-        if verbose == True:
-            print "\n  {}{}{}".format(beautyConsole.getColor(
-                "white"), description, beautyConsole.getSpecialChar("endline"))
-            print "  {}{}{}".format(beautyConsole.getColor(
-                "grey"), syntax, beautyConsole.getSpecialChar("endline"))
-            print "  Potential impact: {}{}{}".format(beautyConsole.getColor(
-                impact_color[impact]), vuln_class, beautyConsole.getSpecialChar("endline"))
-
-        if impact not in severity.keys():
-            severity[impact] = 1
+    def run(self):
+        """
+        runs scanning
+        """
+        if self.recursive:
+            for root, subdirs, files in os.walk(self.filename):
+                for f in files:
+                    self.scanned_files = self.scanned_files + 1
+                    res = self.main(os.path.join(root, f))
+                    self.found_entries = self.found_entries + res
         else:
-            severity[impact] = severity[impact] + 1
+            self.scanned_files = self.scanned_files + 1
+            self.found_entries = self.main(self.filename)
 
-    if verbose == True:
-        print "\n"
-        if prev_prev_line:
-            print str(i-2) + "  " + beautyConsole.getColor("grey") + prev_prev_line + \
-                beautyConsole.getSpecialChar("endline")
-        if prev_line:
-            print str(i-1) + "  " + beautyConsole.getColor("grey") + prev_line + \
-                beautyConsole.getSpecialChar("endline")
-        print str(i) + "  " + beautyConsole.getColor("green") + _line.rstrip() + \
-            beautyConsole.getSpecialChar("endline")
-        if next_line:
-            print str(i+1) + "  " + beautyConsole.getColor("grey") + next_line + \
-                beautyConsole.getSpecialChar("endline")
-        if next_next_line:
-            print str(i+2) + "  " + beautyConsole.getColor("grey") + next_next_line + \
-                beautyConsole.getSpecialChar("endline")
-        print "\n"
-
-
-def header_print(file_name, header_printed):
-    if header_printed == False:
         print beautyConsole.getColor("white") + "-" * 100
-        print "FILE: \33[33m%s\33[0m " % os.path.realpath(file_name), "\n"
-        header_printed = True
-    return header_printed
 
-
-def main(src, severity, verbose=False, sql=False, critical=False):
-    """
-    performs code analysis, line by line
-    """
-    f = open(src, "r")
-    i = 0
-    total = 0
-    filenamelength = len(src)
-    linelength = 97
-    all_lines = f.readlines()
-
-    header_printed = False
-    prev_prev_line = ""
-    prev_line = ""
-    next_line = ""
-    next_next_line = ""
-
-    for l in all_lines:
-        if i > 2:
-            prev_prev_line = all_lines[i - 2].rstrip()
-        if i > 1:
-            prev_line = all_lines[i - 1].rstrip()
-        if i < (len(all_lines) - 1):
-            next_line = all_lines[i + 1].rstrip()
-        if i < (len(all_lines) - 2):
-            next_next_line = all_lines[i + 2].rstrip()
-
-        i += 1
-        line = l.rstrip()
-
-        if critical:
-            for fn in pefdefs.critical:
-                # there has to be space before function call; prevents from false-positives strings contains PHP function names
-                atfn = "@{}".format(fn)
-                fn = " {}".format(fn)
-                # also, it has to checked agains @ at the beginning of the function name
-                # @ prevents from output being echoed
-                if fn in line or atfn in line:
-                    header_printed = header_print(f.name, header_printed)
-                    total += 1
-                    self.print_code_line(l, i, fn + (')' if '(' in fn else ''), prev_line,
-                                         next_line, prev_prev_line, next_next_line, severity,
-                                         verbose)
+        print beautyConsole.getColor("green")
+        print "\n>>>  {} file(s) scanned".format(self.scanned_files)
+        if self.found_entries > 0:
+            print "{}>>>  {} interesting entries found\n".format(
+                beautyConsole.getColor("red"), self.found_entries)
         else:
-            for fn in pefdefs.exploitableFunctions:
-                # there has to be space before function call; prevents from false-positives strings contains PHP function names
-                atfn = "@{}".format(fn)
-                fn = " {}".format(fn)
-                # also, it has to checked agains @ at the beginning of the function name
-                # @ prevents from output being echoed
-                if fn in line or atfn in line:
-                    header_printed = header_print(f.name, header_printed)
-                    total += 1
-                    self.print_code_line(l, i, fn + (')' if '(' in fn else ''), prev_line,
-                                         next_line, prev_prev_line, next_next_line, severity,
-                                         verbose)
+            print "  No interesting entries found :( \n"
 
-        if critical == False:
-            for dp in pefdefs.fileInclude:
-                # there has to be space before function call; prevents from false-positives strings contains PHP function names
-                dp = " {}".format(dp)
-                # remove spaces to allow detection eg. include(  $_GET['something]  )
-                if dp in line.replace(" ", ""):
-                    header_printed = header_print(f.name, header_printed)
-                    total += 1
-                    self.print_code_line(l, i, dp + '()', prev_line, next_line,
-                                         prev_prev_line, next_next_line, severity,
-                                         verbose)
+        print "{}==>  {}:\t {}".format(
+            beautyConsole.getColor("red"), "HIGH", self.severity.get("high"))
+        print "{}==>  {}:\t {}".format(beautyConsole.getColor(
+            "yellow"), "MEDIUM", self.severity.get("medium"))
+        print "{}==>  {}:\t {}".format(beautyConsole.getColor(
+            "green"), "LOW", self.severity.get("low"))
 
-            for globalvars in pefdefs.globalVars:
-                if globalvars in line:
-                    header_printed = header_print(f.name, header_printed)
-                    total += 1
-                    self.print_code_line(l, i, globalvars, prev_line, next_line,
-                                         prev_prev_line, next_next_line, severity,
-                                         verbose)
-
-            for refl in pefdefs.reflectedProperties:
-                if refl in line:
-                    header_printed = header_print(f.name, header_printed)
-                    total += 1
-                    self.print_code_line(l, i, refl, prev_line, next_line,
-                                         prev_prev_line, next_next_line, severity,
-                                         verbose)
-
-            if sql == True:
-                for refl in pefdefs.otherPatterns:
-                    p = re.compile(refl)
-                    if p.search(l):
-                        header_printed = header_print(
-                            f.name, header_printed)
-                        total += 1
-                        self.print_code_line(l, i, refl, prev_line, next_line,
-                                             prev_prev_line, next_next_line, severity,
-                                             verbose)
-
-    if total < 1:
-        pass
-    else:
-        print beautyConsole.getColor("red") + \
-            "Found %d interesting entries\n" % (total) + \
-            beautyConsole.getSpecialChar("endline")
-
-    return total  # return how many findings in current file
+        print "\n"
 
 
 # main program
@@ -437,47 +292,7 @@ if __name__ == "__main__":
     critical = True if args.critical else False
     filename = args.file
 
-    scanned_files = 0
-    found_entries = 0
-
-    severity = {
-        "high": 0,
-        "medium": 0,
-        "low": 0
-    }
-
-    if args.recursive:
-        for root, subdirs, files in os.walk(filename):
-            for f in files:
-                scanned_files = scanned_files + 1
-                res = main(os.path.join(root, f), severity,
-                           verbose, sql, critical)
-                found_entries = found_entries + res
-    else:
-        scanned_files = scanned_files + 1
-        found_entries = main(filename, severity)
-
-    print beautyConsole.getColor("white") + "-" * 100
-
-    print beautyConsole.getColor("green")
-    print "\n>>>  {} file(s) scanned".format(scanned_files)
-    if found_entries > 0:
-        print "{}>>>  {} interesting entries found\n".format(
-            beautyConsole.getColor("red"), found_entries)
-    else:
-        print "  No interesting entries found :( \n"
-
-    print "{}==>  {}:\t {}".format(
-        beautyConsole.getColor("red"), "HIGH", severity.get("high"))
-    print "{}==>  {}:\t {}".format(beautyConsole.getColor(
-        "yellow"), "MEDIUM", severity.get("medium"))
-    print "{}==>  {}:\t {}".format(beautyConsole.getColor(
-        "green"), "LOW", severity.get("low"))
-
-    print "\n"
-
-    # REFACTORED STARTS HERE
     engine = PefEngine(args.recursive, verbose, critical, sql, filename)
-    # engine.run()
+    engine.run()
 
     exit(0)
