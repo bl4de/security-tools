49a43b2ed14a1bb4a75e5233edb3313b77a9c132
Rafal Janicki <rafal.janicki@genomicsmed.ie>
2019-11-04 16:07:55 +0000

    generate JWT [jwt_decoder.py]

--- jwt_decoder.py
+++ jwt_decoder.py
@@ -3,8 +3,13 @@
 # JWT Decoder
 import base64
 import sys
+import hmac
+import hashlib
+import binascii
+
 # eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIxNDE2OTI5MDYxIiwianRpIjoiODAyMDU3ZmY5YjViNGViN2ZiYjg4NTZiNmViMmNjNWIiLCJzY29wZXMiOnsidXNlcnMiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19LCJ1c2Vyc19hcHBfbWV0YWRhdGEiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19fX0.gll8YBKPLq6ZLkCPLoghaBZG_ojFLREyLQYx0l2BG3E
 
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIxNDE2OTI5MDYxIiwianRpIjoiODAyMDU3ZmY5YjViNGViN2ZiYjg4NTZiNmViMmNjNWIiLCJzY29wZXMiOnsidXNlcnMiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19LCJ1c2Vyc19hcHBfbWV0YWRhdGEiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19fX0.15308fa263baaa57c2c84528d913ab75892352d927ccbd29e5af8fd783257996
 
 def get_parts(jwt):
     return dict(zip(['header', 'payload', 'signature'], jwt.split('.')))
@@ -18,13 +23,32 @@ def decode_part(part):
 
 
 def encode_part(part):
-    return base64.urlsafe_b64encode(part).decode('utf-8')
+    return base64.urlsafe_b64encode(part).decode('utf-8').replace('=', '')
+
+
+def build_jwt(header, payload, key, alg='hmac'):
+    message = encode_part(header) + '.' + encode_part(payload)
+    if alg == 'hmac':
+        signature = hmac.new(bytes(key,'ascii'), bytes(message, 'ascii'), hashlib.sha256).hexdigest()
+    elif alg == 'none':
+        # if alg is set to 'none'
+        signature = ''
+    else:
+        pass
+    return f'{message}.{signature}'
 
 
 parts = get_parts(sys.argv[1])
 
 header = decode_part(parts['header'])
-print(header)
+# print(header)
 
 payload = decode_part(parts['payload'])
-print(payload)
+# print(payload)
+
+
+message = encode_part(header) + '.' + encode_part(payload)
+# print(message)
+
+jwt = build_jwt(header, payload, 'key')
+print(jwt)
