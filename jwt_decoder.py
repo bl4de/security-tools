5f1fe4b6789cc3d4144a4b39d96be93c3210c729
Rafal Janicki <rafal.janicki@genomicsmed.ie>
2019-11-04 16:32:29 +0000

    not working :/ [jwt-decoder.py]

--- jwt_decoder.py
+++ jwt_decoder.py
@@ -11,6 +11,7 @@ import binascii
 
 # eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIxNDE2OTI5MDYxIiwianRpIjoiODAyMDU3ZmY5YjViNGViN2ZiYjg4NTZiNmViMmNjNWIiLCJzY29wZXMiOnsidXNlcnMiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19LCJ1c2Vyc19hcHBfbWV0YWRhdGEiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19fX0.15308fa263baaa57c2c84528d913ab75892352d927ccbd29e5af8fd783257996
 
+
 def get_parts(jwt):
     return dict(zip(['header', 'payload', 'signature'], jwt.split('.')))
 
@@ -27,9 +28,10 @@ def encode_part(part):
 
 
 def build_jwt(header, payload, key, alg='hmac'):
-    message = encode_part(header) + '.' + encode_part(payload)
+    message = f'{encode_part(header)}.{encode_part(payload)}'.encode()
     if alg == 'hmac':
-        signature = hmac.new(bytes(key,'ascii'), bytes(message, 'ascii'), hashlib.sha256).hexdigest()
+        signature = hmac.new(key.encode(), message,
+                             hashlib.sha256).hexdigest()
     elif alg == 'none':
         # if alg is set to 'none'
         signature = ''
@@ -41,14 +43,11 @@ def build_jwt(header, payload, key, alg='hmac'):
 parts = get_parts(sys.argv[1])
 
 header = decode_part(parts['header'])
-# print(header)
+print(header)
 
 payload = decode_part(parts['payload'])
-# print(payload)
+print(payload)
 
 
-message = encode_part(header) + '.' + encode_part(payload)
-# print(message)
-
-jwt = build_jwt(header, payload, 'key')
+jwt = build_jwt(header, payload, 'secrety')
 print(jwt)
