3e93b88028078f73d870ef480c49dce36d893ea7
Rafal Janicki <rafal.janicki@genomicsmed.ie>
2019-11-04 15:10:37 +0000

    initial decoding of header and payload of JWT [jwt_decoder.py]

--- jwt_decoder.py
+++ jwt_decoder.py
@@ -2,9 +2,29 @@
 
 # JWT Decoder
 import base64
+import sys
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIxNDE2OTI5MDYxIiwianRpIjoiODAyMDU3ZmY5YjViNGViN2ZiYjg4NTZiNmViMmNjNWIiLCJzY29wZXMiOnsidXNlcnMiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19LCJ1c2Vyc19hcHBfbWV0YWRhdGEiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19fX0.gll8YBKPLq6ZLkCPLoghaBZG_ojFLREyLQYx0l2BG3E
 
-jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIxNDE2OTI5MDYxIiwianRpIjoiODAyMDU3ZmY5YjViNGViN2ZiYjg4NTZiNmViMmNjNWIiLCJzY29wZXMiOnsidXNlcnMiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19LCJ1c2Vyc19hcHBfbWV0YWRhdGEiOnsiYWN0aW9ucyI6WyJyZWFkIiwiY3JlYXRlIl19fX0.gll8YBKPLq6ZLkCPLoghaBZG_ojFLREyLQYx0l2BG3E"
 
-jwt = jwt.split('.')
+def get_parts(jwt):
+    return dict(zip(['header', 'payload', 'signature'], jwt.split('.')))
 
-print(jwt)
+
+def decode_part(part):
+    # use Base64URL decode plus optional padding.
+    # === makes sure that padding will be always correct
+    # extraneous padding is ignored
+    return base64.urlsafe_b64decode(part + '===')
+
+
+def encode_part(part):
+    return base64.urlsafe_b64encode(part).decode('utf-8')
+
+
+parts = get_parts(sys.argv[1])
+
+header = decode_part(parts['header'])
+print(header)
+
+payload = decode_part(parts['payload'])
+print(payload)
