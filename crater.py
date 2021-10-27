41feb56b73dbef078f810c70dd865509709c66ab
Rafal Janicki <bloorq@gmail.com>
2021-10-27 13:58:37 +0100

    [crater] parse crt.sh result set

--- crater.py
+++ crater.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 import sys
 import requests
-
+import json
 
 domain = sys.argv[1]
 base_url = "https://crt.sh/?q={}&output=json".format(domain)
+data = {}
+extracted_domains = []
 
 resp = requests.get(base_url)
 
 if resp.status_code == 200:
-    print(resp.content)
-    
+    data = json.loads(resp.content.decode('utf-8'))
+    for elem in data:
+        if elem['name_value'] not in extracted_domains:
+            extracted_domains.append(elem['name_value'])
+
+    print(extracted_domains)
+else:
+    print("[-] No data retrieved for domain {}".format(domain))
