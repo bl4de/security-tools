11f3d3fdb0314e39bc7758f402dd6d9adc0d1a6d
Rafal Janicki <bloorq@gmail.com>
2021-11-12 22:07:29 +0000

    [s0mbra] Removed logo

--- s0mbra.sh
+++ s0mbra.sh
@@ -24,35 +24,6 @@ CYAN='\033[36m'
 CLR='\033[0m'
 NEWLINE='\n'
 
-
-__logo="
-                      :PB@Bk:
-                  ,jB@@B@B@B@BBL.
-               7G@B@B@BMMMMMB@B@B@Nr
-           :kB@B@@@MMOMOMOMOMMMM@B@B@B1,
-       :5@B@B@B@BBMMOMOMOMOMOMOMM@@@B@B@BBu.
-    70@@@B@B@B@BXBBOMOMOMOMOMOMMBMPB@B@B@B@B@Nr
-  G@@@BJ iB@B@@  OBMOMOMOMOMOMOM@2  B@B@B. EB@B@S
-  @@BM@GJBU.  iSuB@OMOMOMOMOMOMM@OU1:  .kBLM@M@B@
-  B@MMB@B       7@BBMMOMOMOMOMOBB@:       B@BMM@B
-  @@@B@B         7@@@MMOMOMOMM@B@:         @@B@B@
-  @@OLB.          BNB@MMOMOMM@BEB          rBjM@B
-  @@  @           M  OBOMOMM@q  M          .@  @@
-  @@OvB           B:u@MMOMOMMBJiB          .BvM@B
-  @B@B@J         0@B@MMOMOMOMB@B@u         q@@@B@
-  B@MBB@v       G@@BMMMMMMMMMMMBB@5       F@BMM@B
-  @BBM@BPNi   LMEB@OMMMM@B@MMOMM@BZM7   rEqB@MBB@
-  B@@@BM  B@B@B  qBMOMB@B@B@BMOMBL  B@B@B  @B@B@M
-   J@@@@PB@B@B@B7G@OMBB.   ,@MMM@qLB@B@@@BqB@BBv
-      iGB@,i0@M@B@MMO@E  :  M@OMM@@@B@Pii@@N:
-         .   B@M@B@MMM@B@B@B@MMM@@@M@B
-             @B@B.i@MBB@B@B@@BM@::B@B@
-             B@@@ .B@B.:@B@ :B@B  @B@O
-               :0 r@B@  B@@ .@B@: P:
-                   vMB :@B@ :BO7
-                       ,B@B
-"
-
 # config commands
 set_ip() {
     export IP="$1"
@@ -481,7 +452,7 @@ pwn() {
 
 cmd=$1
 clear
-# echo "$__logo"
+
 case "$cmd" in
     pwn)
         pwn "$2"
