fe1efd4622273f9c7add9c8c6988148248a90945
Rafal Janicki <bloorq@gmail.com>
2022-06-05 12:18:59 +0100

    [redirgen.py] Refactoring (in progress)

--- redir_gen/redirgen.py
+++ redir_gen/redirgen.py
@@ -3,12 +3,30 @@
 
 import re
 import argparse
+import string
+
+
+def generate(payload, target, dest, param=None) -> str:
+    '''
+        generates and returns single payload
+    '''
+    payload = payload.rstrip()
+    payload = re.sub("TARGET", target, payload)
+    payload = re.sub("DEST", dest, payload)
+    # if param:
+    #     payload = re.sub("PARAM", param, payload)
+    return payload
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--target", "-t", action="store", help="Enter the target address", required=True)
+parser.add_argument("--target", "-t", action="store",
+                    help="Enter the target address", required=True)
 parser.add_argument("--dest", "-d", action="store", help="Enter the address where you want to redirect to",
                     required=True)
-parser.add_argument("--output", "-o", action="store", help="Enter output file name")
+parser.add_argument("--output", "-o", action="store",
+                    help="Enter output file name")
+# parser.add_argument("--param", "-p", action="store",
+#                     help="Vulnerable parameter")
 args = parser.parse_args()
 
 payloads = []
@@ -17,14 +35,11 @@ payloads = []
 junk = re.compile(r"https?://")
 target = junk.sub("", args.target)
 dest = junk.sub("", args.dest)
-
 with open("payloads.txt", "r") as handle:
     templates = handle.readlines()
 
 for payload in templates:
-    payload = payload.rstrip()
-    payload = re.sub("TARGET", target, payload)
-    payload = re.sub("DEST", dest, payload)
+    payload = generate(payload, target, dest)
     print(payload)
     payloads.append(payload)
 
